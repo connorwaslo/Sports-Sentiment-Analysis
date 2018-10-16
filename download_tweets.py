885873209304bc4ba585c8f242e492feab939aeb
Connor Waslo <connorwaslo@gmail.com>
2018-10-16 15:15:56 -0500

    Download #celtics tweets
    Download the last 30 #celtics tweets using Tweepy and Twitter API. Ignore retweets.

--- download_tweets.py
+++ download_tweets.py
@@ -11,17 +11,3 @@ for tweet in tweepy.Cursor(api.search, q='#celtics', f='tweets').items(30):
     if not tweet.retweeted and 'RT @' not in tweet.text:
         print(tweet.text)
         print('-----\n')
-
-# https://twitter.com/search?f=tweets&vertical=news&q=%23celtics&src=typd
-# https://twitter.com/search?vertical=news&q=%23celtics&src=typd
-
-# auth.set_access_token(creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
-#
-# api = tweepy.API(auth)
-#
-# me = api.me()
-# print('Name:', me.name)
-#
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text)
