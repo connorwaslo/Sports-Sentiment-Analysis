e7cba5043d2a65817ca65df1b95ec3c9c07b0518
Connor Waslo <connorwaslo@gmail.com>
2018-10-16 18:04:01 -0500

    3 API calls to grab celtics and sixers tweets
    Loop 3 times to grab some tweets for each team before the game starts.

--- download_tweets.py
+++ download_tweets.py
@@ -1,6 +1,7 @@
 import tweepy
 import json
 import unicodedata
+import time
 
 with open('json/twitter_credentials.json', 'r') as file:
     creds = json.load(file)
@@ -8,16 +9,32 @@ with open('json/twitter_credentials.json', 'r') as file:
 auth = tweepy.OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
 api = tweepy.API(auth)
 
-tweets = []
+celtics = []
+sixers = []
 
-for tweet in tweepy.Cursor(api.search, q='#celtics', f='tweets', tweet_mode='extended').items(50):
-    if not tweet.retweeted and 'RT @' not in tweet.full_text:
-        print(tweet.full_text)
-        tweets.append(
-            unicodedata.normalize('NFKD', tweet.full_text).encode('ascii', 'ignore').decode('utf-8') + '\n***\n'
-        )
+iter = 0
+
+while iter < 3:
+    for tweet in tweepy.Cursor(api.search, q='#celtics', f='tweets', tweet_mode='extended').items(90):
+        if not tweet.retweeted and 'RT @' not in tweet.full_text:
+            print(tweet.full_text)
+            celtics.append(
+                unicodedata.normalize('NFKD', tweet.full_text).encode('ascii', 'ignore').decode('utf-8') + '\n***\n'
+            )
+    for tweet in tweepy.Cursor(api.search, q='#sixers', f='tweets', tweet_mode='extended').items(90):
+        if not tweet.retweeted and 'RT @' not in tweet.full_text:
+            print(tweet.full_text)
+            sixers.append(
+                unicodedata.normalize('NFKD', tweet.full_text).encode('ascii', 'ignore').decode('utf-8') + '\n***\n'
+            )
+
+    time.sleep(902)
 
 # Save tweets to text file for manual classification
 with open('tweets/celtics.txt', 'w') as f:
-    for t in tweets:
+    for t in celtics:
+        f.write(t)
+
+with open('tweets/sixers.txt', 'w') as f:
+    for t in sixers:
         f.write(t)
