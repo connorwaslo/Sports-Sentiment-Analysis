9898bb660aa332b69c657cbb8c0c65d19e0a88dd
Connor Waslo <connorwaslo@gmail.com>
2018-10-22 12:38:32 -0500

    Test accuracy of Vader
    Downloaded more tweets for Knicks and Mavs. Tested accuracy of nltk Vader on documents. 52.7%... not great.

--- download_tweets.py
+++ download_tweets.py
@@ -9,22 +9,22 @@ with open('json/twitter_credentials.json', 'r') as file:
 auth = tweepy.OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
 api = tweepy.API(auth)
 
-celtics = []
-sixers = []
+knicks = []
+mavs = []
 
 iter = 0
 
 while iter < 3:
-    for tweet in tweepy.Cursor(api.search, q='#celtics', f='tweets', tweet_mode='extended').items(90):
+    for tweet in tweepy.Cursor(api.search, q='#knicks', f='tweets', tweet_mode='extended').items(90):
         if not tweet.retweeted and 'RT @' not in tweet.full_text:
             print(tweet.full_text)
-            celtics.append(
+            knicks.append(
                 unicodedata.normalize('NFKD', tweet.full_text).encode('ascii', 'ignore').decode('utf-8') + '\n***\n'
             )
-    for tweet in tweepy.Cursor(api.search, q='#sixers', f='tweets', tweet_mode='extended').items(90):
+    for tweet in tweepy.Cursor(api.search, q='#mavs', f='tweets', tweet_mode='extended').items(90):
         if not tweet.retweeted and 'RT @' not in tweet.full_text:
             print(tweet.full_text)
-            sixers.append(
+            mavs.append(
                 unicodedata.normalize('NFKD', tweet.full_text).encode('ascii', 'ignore').decode('utf-8') + '\n***\n'
             )
 
@@ -32,10 +32,10 @@ while iter < 3:
     iter += 1
 
 # Save tweets to text file for manual classification
-with open('tweets/celtics.txt', 'w') as f:
-    for t in celtics:
+with open('tweets/knicks.txt', 'w') as f:
+    for t in knicks:
         f.write(t)
 
-with open('tweets/sixers.txt', 'w') as f:
-    for t in sixers:
+with open('tweets/mavs.txt', 'w') as f:
+    for t in mavs:
         f.write(t)
--- test_sentiment.py
+++ test_sentiment.py
@@ -1,3 +1,27 @@
 import sentiment_analyzer as s
+import pickle
 
-print(s.sentiment("I'M SHAKING LET'S GOOOOOOOOOOOOOOOOO CELTICS #CUsRise #Celtics"))
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+sid = SentimentIntensityAnalyzer()
+
+# Test accuracy of Vader sentiment analysis
+documents_f = open('pickles/documents.pickle', 'rb')
+documents = pickle.load(documents_f)
+documents_f.close()
+
+# Use same testing set as other classifiers
+testing_set = documents[:1000]
+
+correct = 0
+
+for doc in testing_set:
+    analysis = sid.polarity_scores(doc[0])
+    if analysis['compound'] > 0 and doc[1] == 'pos':
+        correct += 1
+    elif analysis['compound'] < 0 and doc[1] == 'neg':
+        correct += 1
+
+print('Accuracy:', str((correct / 1000) * 100) + '%')
+
+# print(s.sentiment("I'M SHAKING LET'S GOOOOOOOOOOOOOOOOO CELTICS #CUsRise #Celtics"))
