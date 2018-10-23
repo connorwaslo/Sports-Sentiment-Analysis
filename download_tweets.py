f91601c0f47c3c4e67deff6060b2b7eba0676ac8
Connor Waslo <connorwaslo@gmail.com>
2018-10-23 15:09:40 -0500

    Download tweets by date
    Downloaded warriors tweets in date range.

--- download_tweets.py
+++ download_tweets.py
@@ -2,6 +2,7 @@ import tweepy
 import json
 import unicodedata
 import time
+import csv
 
 with open('json/twitter_credentials.json', 'r') as file:
     creds = json.load(file)
@@ -9,33 +10,59 @@ with open('json/twitter_credentials.json', 'r') as file:
 auth = tweepy.OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
 api = tweepy.API(auth)
 
-knicks = []
-mavs = []
+# knicks = []
+# mavs = []
+
+tweets = []
 
 iter = 0
 
-while iter < 3:
-    for tweet in tweepy.Cursor(api.search, q='#knicks', f='tweets', tweet_mode='extended').items(90):
-        if not tweet.retweeted and 'RT @' not in tweet.full_text:
-            print(tweet.full_text)
-            knicks.append(
-                unicodedata.normalize('NFKD', tweet.full_text).encode('ascii', 'ignore').decode('utf-8') + '\n***\n'
-            )
-    for tweet in tweepy.Cursor(api.search, q='#mavs', f='tweets', tweet_mode='extended').items(90):
-        if not tweet.retweeted and 'RT @' not in tweet.full_text:
-            print(tweet.full_text)
-            mavs.append(
-                unicodedata.normalize('NFKD', tweet.full_text).encode('ascii', 'ignore').decode('utf-8') + '\n***\n'
-            )
+for tweet in tweepy.Cursor(api.search, q='#gsw', f='tweets', tweet_mode='extended',
+                           since='2018-10-21', until='2018-10-23', lang='en').items():
+    if iter == 179:
+        with open('tweets/warriors.csv', 'a') as f:
+            writer = csv.writer(f)
+            for t in tweets:
+                writer.writerow([t])
 
-    time.sleep(902)
+        # Empty tweets list
+        tweets = []
+
+        time.sleep(901)
+        iter = 0
+
+    print(tweet.full_text)
+    tweets.append(
+        unicodedata.normalize('NFKD', tweet.full_text).encode('ascii', 'ignore').decode('utf-8') + '\n***\n'
+    )
+
     iter += 1
 
-# Save tweets to text file for manual classification
-with open('tweets/knicks.txt', 'w') as f:
-    for t in knicks:
-        f.write(t)
+if len(tweets) > 0:
+    with open('tweets/warriors.csv', 'a') as f:
+        writer = csv.writer(f)
+        for t in tweets:
+            writer.writerow([t])
 
-with open('tweets/mavs.txt', 'w') as f:
-    for t in mavs:
-        f.write(t)
+# while iter < 3:
+#     for tweet in tweepy.Cursor(api.search, q='#knicks', f='tweets', tweet_mode='extended').items(90):
+#         if not tweet.retweeted and 'RT @' not in tweet.full_text:
+#             print(tweet.full_text)
+#             knicks.append(
+#                 unicodedata.normalize('NFKD', tweet.full_text).encode('ascii', 'ignore').decode('utf-8') + '\n***\n'
+#             )
+#     for tweet in tweepy.Cursor(api.search, q='#mavs', f='tweets', tweet_mode='extended').items(90):
+#         if not tweet.retweeted and 'RT @' not in tweet.full_text:
+#             print(tweet.full_text)
+#             mavs.append(
+#                 unicodedata.normalize('NFKD', tweet.full_text).encode('ascii', 'ignore').decode('utf-8') + '\n***\n'
+#             )
+#
+#     time.sleep(902)
+#     iter += 1
+
+# Save tweets to text file for manual classification
+
+# with open('tweets/mavs.txt', 'w') as f:
+#     for t in mavs:
+#         f.write(t)
--- test_sentiment.py
+++ test_sentiment.py
@@ -1,9 +1,18 @@
 import sentiment_analyzer as s
 import pickle
+import csv
+import re
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 sid = SentimentIntensityAnalyzer()
+
+pos_tweets = []
+with open('testing_data/nba_tweets.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        tweet = row[0]
+        pos_tweets.append(re.sub('&.+?;', '', tweet)) # Delete words like &quot;
 
 # Test accuracy of Vader sentiment analysis
 documents_f = open('pickles/documents.pickle', 'rb')
@@ -11,17 +20,37 @@ documents = pickle.load(documents_f)
 documents_f.close()
 
 # Use same testing set as other classifiers
-testing_set = documents[:1000]
+testing_set = pos_tweets #  documents[:1000]
 
-correct = 0
+# print(testing_set[0][0], testing_set[0][1])
+# print(s.sentiment(testing_set[0][0]))
+
+vader_correct = 0
+vote_correct = 0
 
 for doc in testing_set:
-    analysis = sid.polarity_scores(doc[0])
-    if analysis['compound'] > 0 and doc[1] == 'pos':
-        correct += 1
-    elif analysis['compound'] < 0 and doc[1] == 'neg':
-        correct += 1
+    vader_analysis = sid.polarity_scores(doc)
+    vote_analysis = s.sentiment(doc)
+    if vader_analysis['compound'] > 0:
+        vader_correct += 1
 
-print('Accuracy:', str((correct / 1000) * 100) + '%')
+    if vote_analysis[0] == 'pos':
+        vote_correct += 1
+
+# for doc in testing_set:
+#     vader_analysis = sid.polarity_scores(doc[0])
+#     vote_analysis = s.sentiment(doc[0])
+#     if vader_analysis['compound'] > 0 and doc[1] == 'pos':
+#         vader_correct += 1
+#     elif vader_analysis['compound'] < 0 and doc[1] == 'neg':
+#         vader_correct += 1
+#
+#     if vote_analysis[0] == 'pos' and doc[1] == 'pos':
+#         vote_correct += 1
+#     elif vote_analysis[0] == 'neg' and doc[1] == 'neg':
+#         vote_correct += 1
+
+print('Vader accuracy:', str((vader_correct / 46) * 100) + '%')
+print('VoteClassifier accuracy:', str((vote_correct / 46) * 100) + '%')
 
 # print(s.sentiment("I'M SHAKING LET'S GOOOOOOOOOOOOOOOOO CELTICS #CUsRise #Celtics"))
